d3f030f22466f3e743cfd41613815c1414156d00
lebedevmoscow <b.syrpin@sberauto.com>
2021-10-30 23:18:34 +0500

    controller routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
 import {CreateUserDTO} from "./dtos/create-user.dto";
 import {UsersService} from "./users.service";
+import {UpdateUserDto} from "./dtos/update-user.dto";
 
 @Controller('auth')
 export class UsersController {
@@ -11,4 +12,23 @@ export class UsersController {
         return this.userService.create(body.email, body.password)
     }
 
+    @Get('/:id')
+    public findUser(@Param('id') id: string) {
+        return this.userService.findOne(parseInt(id))
+    }
+
+    @Get()
+    public findAllUser(@Query('email') email: string) {
+        return this.userService.find(email)
+    }
+
+    @Delete('/:id')
+    public removeUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id))
+    }
+
+    @Patch('/:id')
+    public updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.userService.update(parseInt(id), body)
+    }
 }
