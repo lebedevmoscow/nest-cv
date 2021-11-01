1f058e20d6b89d5c270104b507bddb5885546100
lebedevmoscow <b.syrpin@sberauto.com>
2021-11-01 21:19:26 +0500

    basic interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,22 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    NotFoundException,
+    Param,
+    Patch,
+    Post,
+    Query
+} from '@nestjs/common';
 import {CreateUserDTO} from "./dtos/create-user.dto";
 import {UsersService} from "./users.service";
 import {UpdateUserDto} from "./dtos/update-user.dto";
+import {UserDto} from "./dtos/user.dto";
+import {Serialize} from "../interceptors/serialize.interceptor";
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private userService: UsersService) {}
 
@@ -13,8 +26,14 @@ export class UsersController {
     }
 
     @Get('/:id')
-    public findUser(@Param('id') id: string) {
-        return this.userService.findOne(parseInt(id))
+    public async findUser(@Param('id') id: string) {
+        const user = await this.userService.findOne(parseInt(id))
+
+        if (!user) {
+            throw new NotFoundException('user not found')
+        }
+
+        return user
     }
 
     @Get()
