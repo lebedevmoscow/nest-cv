739192b92be5634c2bd289db6af637e8f0c6bdc1
lebedevmoscow <b.syrpin@sberauto.com>
2021-11-02 20:22:33 +0500

    simple auth service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,15 +14,21 @@ import {UsersService} from "./users.service";
 import {UpdateUserDto} from "./dtos/update-user.dto";
 import {UserDto} from "./dtos/user.dto";
 import {Serialize} from "../interceptors/serialize.interceptor";
+import {AuthService} from "./auth.service";
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-    constructor(private userService: UsersService) {}
+    constructor(private userService: UsersService, private authService: AuthService) {}
 
-    @Post('signup')
+    @Post('/signup')
     public createUser(@Body() body: CreateUserDTO) {
-        return this.userService.create(body.email, body.password)
+        return this.authService.signup(body.email, body.password)
+    }
+
+    @Post('/signin')
+    public signin(@Body() body: CreateUserDTO) {
+        return this.authService.signin(body.email, body.password)
     }
 
     @Get('/:id')
